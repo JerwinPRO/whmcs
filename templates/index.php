04c05ca9ad905b37fe1a36913b07f3538e3761b0
Jerwin Pastoral <project@jerwinpastoral.com>
2013-07-03 20:45:31 +0800

    WHMCS version 5.2.5

---
+++ templates/index.php
@@ -0,0 +1,42 @@
+<?php //00e0a
+// *************************************************************************
+// *                                                                       *
+// * WHMCS - The Complete Client Management, Billing & Support Solution    *
+// * Copyright (c) WHMCS Ltd. All Rights Reserved,                         *
+// * Version: 5.2.4 Stable                                                 *
+// * Release Date: 23rd April 2013                                         *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * Email: info@whmcs.com                                                 *
+// * Website: http://www.whmcs.com                                         *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * This software is furnished under a license and may be used and copied *
+// * only  in  accordance  with  the  terms  of such  license and with the *
+// * inclusion of the above copyright notice.  This software  or any other *
+// * copies thereof may not be provided or otherwise made available to any *
+// * other person.  No title to and  ownership of the  software is  hereby *
+// * transferred.                                                          *
+// *                                                                       *
+// * You may not reverse  engineer, decompile, defeat  license  encryption *
+// * mechanisms, or  disassemble this software product or software product *
+// * license.  WHMCompleteSolution may terminate this license if you don't *
+// * comply with any of the terms and conditions set forth in our end user *
+// * license agreement (EULA).  In such event,  licensee  agrees to return *
+// * licensor  or destroy  all copies of software  upon termination of the *
+// * license.                                                              *
+// *                                                                       *
+// * Please see the EULA file for the full End User License Agreement.     *
+// *                                                                       *
+// *************************************************************************
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPzvtbYUjtce8hCG31Ay6H9E7EyESxlbiFSyGWjhWXVQEx8ZhtC5ZELsxhAQhW/qWxlCEQx5X
+rKDdSD/27rd4BhcHVaV0ZW+497UygF977EFe85JT9eJM7Uz7S14fIoWJLLk9hL9k4fy7oxlb1O41
+VGQ9tOfwdh0epHvH/YWL19OJNkXqW40OMrjrikuupCA3i6k1IPhXqRSYnwS4XDlEPImcawe8Y86D
+mXk8vme8zi4lU2gjAhzntenEnLrFxycN3BcGQ3xPNW3BNuFitOgkEiHhuj06LI1Lzch7wF6p5He4
+SGMKvIf4c54YtuLxwgGArgoBpMku2Ap/3mEh/FQ7GgtmvNAvyXapg4TF99596MMu4QA3qj1PsJrZ
+CAVnzpv9ecp6FrVkSWa8mQ6ANDNJ8UTShBzROZ088/joSAEsICvxBc+v2Fu1rsxp72gSYlansdhj
+iseVIAXRFkv9di11E41Mkr5/YJOZjrTUk6Z90s0RR/tFDBfSHYWT
